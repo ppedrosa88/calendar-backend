b7580d944d2e46e1ccd5533ba03f1f37a4b6e7af
Pablo Pedrosa <ppedrosabutron@gmail.com>
2023-10-18 16:02:51 +0200

    CORS config3

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ dbConnection()
 // Configurar cabeceras y cors
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', 'https://https://calendar-app-pied.vercel.app/');
   res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
   res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
